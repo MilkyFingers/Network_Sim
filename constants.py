a055a2f15accdbb9d7c7915273904de680cd6efb
wtljohnson <johnsonw167@gmail.com>
2024-01-09 00:07:17 +0000

    Added mac addresses to constants

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 SERVER_IP = "192.168.1.0"
 SERVER_NAME = "networksimulation.com"
+SERVER_MAC = "02:00:00:00:00:00" # (locally administered unicast address)
 CLIENT_IP = "192.168.1.1"
+CLIENT_MAC = "02:00:00:00:00:01" # (locally administered unicast address)
 NETMASK = "255.0.0.0"
 HTTP_SERVER2CLIENT = "http_pipe_S2C"
 HTTP_CLIENT2SERVER = "http_pipe_C2S"
