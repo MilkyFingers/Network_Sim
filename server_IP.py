06ba97db1efca8a2f96a34c68566395801c230cf
MilkyFingers <micahbassett9@gmail.com>
2024-01-24 13:39:21 +0000

    finished ip class

--- server_IP.py
+++ server_IP.py
@@ -2,17 +2,48 @@ import constants
 
 class IP:
 
-    def __init__(self):
-        pass
+    def __init__(self, ip = constants.SERVER_IP):
+        self.ip = ip
 
-    # this method will take a datagram extracted from the link layer, and extract the network layer datagram
-    def network_to_transport(self, data):
-        tcp_datagram = data[160:]
-        return tcp_datagram
+    """
+    This method will unpack the IP packet, check the destination IP is correct as well as the checksum and return the tcp segment if the 
+    checks pass. 
+    """
+    def network_to_transport(self, packet):
+        # extract the ip 
+        dest_ip = packet[160:192]
+        # get the checksum
+        checksum = packet[80:96]
+        # tcp segment
+        segment = packet[192:]
+        # drop the packet if the checks fail
+        if (dest_ip != constants.ip_to_bin(self.ip)) or (checksum != constants.calculate_checksum(packet[:192])):
+            pass
+        else:
+            return segment
     
     # Version|IHL|DSCP|ECN|Total Length|Identification|Flags|Fragment offset|Time to Live(linux kernel 4.10+)|Protocol|Header checksum
     # |Source address|Destination address|Payload
     def network_to_link(self, tcp):
-        ip = f"{4:04b}{5:04b}{0:06b}{0:02b}{160+len(tcp):016b}{1:016b}010{0:013b}{64:08b}{6:08b}{0:016b}" \
-             f"{192:08b}{168:08b}{1:08b}{1:08b}{192:08b}{168:08b}{1:08b}{0:08b}{constants.strtobin(tcp)}"
-        return ip
+        # variables for all the headers
+        version = f"{4:04b}"
+        ihl = f"{5:04b}"
+        service_type = f"{0:08b}"
+        total_length = f"{str(len(tcp) + 160):016b}"
+        iden = f"{1:016b}"
+        flags = '010'
+        fragment_off = f'{0:013b}'
+        ttl = f"{64:08b}"
+        transport = f"{6:08b}"
+        #initially set checksum to 0
+        check = f"{0:016b}"
+        send_ip = constants.ip_to_bin(self.ip)
+        dest_ip = constants.ip_to_bin(constants.CLIENT_IP)
+        
+        # create a header
+        header = version + ihl + service_type + total_length + iden + flags + fragment_off + ttl + transport + check + send_ip + dest_ip
+        # calculate the correct checksum
+        check = constants.calculate_checksum(header)
+        header = version + ihl + service_type + total_length + iden + flags + fragment_off + ttl + transport + check + send_ip + dest_ip
+        return header + tcp
+
